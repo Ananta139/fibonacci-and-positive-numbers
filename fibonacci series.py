e29e9b867bba388b9ac1317a1293f10344d26269
Ananta139 <69521069+Ananta139@users.noreply.github.com>
2020-08-13 00:25:39 +0530

    Add files via upload

---
+++ fibonacci series.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#fibonacci series
+x=0
+y=1
+z=0
+n= input("enter no. of digits you want to see: ")
+for i in range(0,int(n)):
+    print(z, end=" ")
+    z=x+y
+    x=y
+    y=z
+    
+
+
+# In[13]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ positive no.s
@@ -0,0 +1,60 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "9 6 7 8 "
+     ]
+    }
+   ],
+   "source": [
+    "#selecting positive numbers from a list\n",
+    "list=[-10,9,-4,-5,6,7,8,-19]\n",
+    "for n in list:\n",
+    "    if n>=0:\n",
+    "        print (n, end=' ')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.1"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
